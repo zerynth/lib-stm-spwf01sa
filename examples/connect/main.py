3ddf9cd919ff1d499f13cc0faeb47962ac9934e5
Zeryntth <dev@zerynth.com>
2020-09-24 15:41:42 +0000

    r2.6.1-base

--- examples/connect/main.py
+++ examples/connect/main.py
@@ -12,7 +12,8 @@ try:
    # Wifi 4 Click on slot B (specify which serial port will be used and which RST pin
     wifi_driver.init(SERIAL1,D16, baud=9600)
 except Exception as e:
-    print(e)for i in range(0,5):
+    print(e)
+for i in range(0,5):
     try:
         # connect to the wifi network (Set your SSID and password below)
         wifi.link(SSID , wifi.WIFI_WPA2, PASSWORD)
